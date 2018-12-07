baac318fc9987774767d9f5619fa4bc1a2213ba4
Irina Serova <isserova@gmail.com>
2018-12-07 12:02:57 -0800

    first commit

---
+++ Calc.java
@@ -0,0 +1,33 @@
+
+public class Calc {
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		if (b == 0) {
+			System.out.println("Error! Dividing by zero is not allowed.");
+			return 0;
+		}
+		else { 
+			return a / b;
+		}
+	}
+
+	public static void main(String[] args) {
+		Calc myCalculator = new Calc();
+		System.out.println(myCalculator.add(5,7));
+		System.out.println(myCalculator.subtract(45,11));
+		
+	}
+
+
+}
